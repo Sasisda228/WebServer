4fb0d7181d3807b776ce1a6ca1740d37b9e46422
Denat <43885390+Sasisda228@users.noreply.github.com>
2025-04-20 23:54:47 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
       name: "FrontEnd",
       script: "node_modules/next/dist/bin/next",
       args: "-p 4000",
-      exec_mode: "cluster",
-      instances: "max",
     },
   ],
 };
