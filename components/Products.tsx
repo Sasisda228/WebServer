ef082e0c235d671d37cc5c6514c9fa2e9bf4dea5
Denat <43885390+Sasisda228@users.noreply.github.com>
2025-04-22 23:56:36 +0300

    Update Products.tsx

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,7 +1,7 @@
 "use client";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import ProductItem from "./ProductItem";
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ProductItem from "./ProductItem"
 
 const Products = ({ slug }: any) => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -59,18 +59,21 @@ const Products = ({ slug }: any) => {
   }
 
   return (
-    <div className="grid grid-cols-3 justify-items-center gap-x-2 gap-y-5 max-[1300px]:grid-cols-3 max-lg:grid-cols-2 max-[500px]:grid-cols-2">
-      {products.length > 0 ? (
-        products.map((product: Product) => (
-          <ProductItem key={product.id} product={product} />
-        ))
-      ) : (
-        <h3 className="text-3xl mt-5 text-center w-full col-span-full max-[1000px]:text-2xl max-[500px]:text-lg">
-          No products found for specified query
-        </h3>
-      )}
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-7xl px-4">
+        <div className="grid grid-cols-3 gap-x-6 gap-y-8 justify-items-center max-[1300px]:grid-cols-3 max-lg:grid-cols-2 max-[500px]:grid-cols-2">
+          {products.length > 0 ? (
+            products.map((product: Product) => (
+              <ProductItem key={product.id} product={product} />
+            ))
+          ) : (
+            <h3 className="text-3xl mt-5 text-center w-full col-span-full max-[1000px]:text-2xl max-[500px]:text-lg">
+              No products found for specified query
+            </h3>
+          )}
+        </div>
+      </div>
     </div>
   );
-};
 
 export default Products;
