3b0a919e79de4f5a9ea577d51ce8e3f01f70dda7
Denat <43885390+Sasisda228@users.noreply.github.com>
2025-04-26 00:01:58 +0300

    tried

--- app/shop/[[...slug]]/page.tsx
+++ app/shop/[[...slug]]/page.tsx
@@ -1,32 +1,82 @@
-"use client";
-
-import { Filters, Pagination, Products } from "@/components";
+import Products from "@/components/Products";
+import dynamic from "next/dynamic";
 import styles from "./ShopPage.module.css";
+const Filters = dynamic(() => import("@/components/Filters"), { ssr: false });
+const Pagination = dynamic(() => import("@/components/Pagination"), {
+  ssr: false,
+});
 
-// improve readability of category text, for example category text "smart-watches" will be "smart watches"
+interface Props {
+  params: { slug: string };
+  searchParams: { [key: string]: string | undefined };
+}
 
-const ShopPage = (slug: any) => {
+async function getProducts(
+  params: Props["params"],
+  searchParams: Props["searchParams"]
+) {
+  const price = searchParams.price ?? "3000";
+  const rating = searchParams.rating ?? "0";
+  const sort = searchParams.sort ?? "id:desc";
+  const page = searchParams.page ?? "1";
+
+  const inStockNum = searchParams.inStock === "true" ? 1 : 0;
+  const outOfStockNum = searchParams.outOfStock === "true" ? 1 : 0;
+
+  let stockMode = "lte";
+  if (inStockNum === 1 && outOfStockNum === 0) stockMode = "equals";
+  if (inStockNum === 0 && outOfStockNum === 1) stockMode = "lt";
+  if (inStockNum === 0 && outOfStockNum === 0) stockMode = "gt";
+
+  const category = params.slug;
+
+  const query = new URLSearchParams({
+    [`filters[price][$lte]`]: price,
+    [`filters[rating][$gte]`]: rating,
+    [`filters[inStock][$${stockMode}]`]: "1",
+    [`filters[category][$equals]`]: category,
+    sort,
+    page,
+  });
+  ///apiv3/products?...
+  const res = await fetch(
+    `http://http://212.67.12.199:3001/api/products?${query}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) return [];
+
+  const data = await res.json();
+  return data;
+}
+
+export default async function ShopPage({ params, searchParams }: Props) {
+  const formattedSlug = params.slug.replace(/-/g, " ");
+  const products = await getProducts(params, searchParams);
+
   return (
     <div className={styles.root}>
       <div className={styles.container}>
+        <div className={styles.divider}></div>
+
         <div className={styles.grid}>
           <div>
-            <div className={styles.divider}></div>
-
             <div className={styles.titleWrapper}>
               <h1 className={styles.title}>
                 <span className={styles.titleGradient}>47STORE</span>
                 <br />
+                {formattedSlug}
               </h1>
             </div>
+
             <Filters />
-            <Products slug={slug} />
+            <Products products={products} />
             <Pagination />
           </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default ShopPage;
+}
--- components/Products.tsx
+++ components/Products.tsx
@@ -1,62 +1,11 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// components/Products.tsx (без "use client")
 import ProductItem from "./ProductItem";
 
-const Products = ({ slug }: any) => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
+interface Props {
+  products: Product[];
+}
 
-  useEffect(() => {
-    let isMounted = true;
-    const fetchProducts = async () => {
-      setLoading(true);
-
-      const inStockNum = slug?.searchParams?.inStock === "true" ? 1 : 0;
-      const outOfStockNum = slug?.searchParams?.outOfStock === "true" ? 1 : 0;
-      const page = slug?.searchParams?.page
-        ? Number(slug?.searchParams?.page)
-        : 1;
-
-      let stockMode: string = "lte";
-      if (inStockNum === 1) stockMode = "equals";
-      if (outOfStockNum === 1) stockMode = "lt";
-      if (inStockNum === 1 && outOfStockNum === 1) stockMode = "lte";
-      if (inStockNum === 0 && outOfStockNum === 0) stockMode = "gt";
-
-      try {
-        const response = await axios.get(
-          `/apiv3/products?filters[price][$lte]=${
-            slug?.searchParams?.price || 3000
-          }&filters[rating][$gte]=${
-            Number(slug?.searchParams?.rating) || 0
-          }&filters[inStock][$${stockMode}]=1&${
-            slug?.params?.slug?.length > 0
-              ? `filters[category][$equals]=${slug?.params?.slug}&`
-              : ""
-          }sort=${slug?.searchParams?.sort}&page=${page}`
-        );
-        if (isMounted) setProducts(response.data);
-      } catch (error) {
-        if (isMounted) setProducts([]);
-      } finally {
-        if (isMounted) setLoading(false);
-      }
-    };
-
-    fetchProducts();
-    return () => {
-      isMounted = false;
-    };
-  }, [slug]);
-
-  if (loading) {
-    return (
-      <div className="w-full text-center py-10">
-        <span>Загрузка товаров...</span>
-      </div>
-    );
-  }
-
+export default function Products({ products }: Props) {
   return (
     <div className="grid grid-cols-3 justify-items-center gap-x-2 gap-y-5 max-[1300px]:grid-cols-3 max-lg:grid-cols-2 max-[500px]:grid-cols-2 ">
       {products.length > 0 ? (
@@ -70,6 +19,4 @@ const Products = ({ slug }: any) => {
       )}
     </div>
   );
-};
-
-export default Products;
+}
