3e724b8c89e8f9a8e65e7da15dda6a174408c807
Denat <43885390+Sasisda228@users.noreply.github.com>
2025-04-25 21:16:21 +0300

    Update ProductItem.tsx

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,13 +1,13 @@
 "use client";
 import SingleProductModal from "@/components/SingleProductModal"; // путь поправьте под ваш проект
-import Image from "next/image";
-import React, { useState } from "react";
+import UploadcareImage from "@uploadcare/nextjs-loader";
+import { useEffect, useState } from "react";
 import styles from "./ProductCard.module.css";
 
 interface Product {
   id: string | number;
   title: string;
-  mainImage: string;
+  images: string[];
   price: number;
   rating?: number;
   slug: string;
@@ -15,6 +15,19 @@ interface Product {
 
 const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [albumGroupId, setAlbumGroupId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const firstImage = product.images[0];
+
+    if (firstImage.includes("ucarecdn.com") && firstImage.includes("/")) {
+      // Извлекаем ID альбома из URL
+      const match = firstImage.match(/ucarecdn\.com\/([^\/]+)/);
+      if (match && match[1]) {
+        setAlbumGroupId(match[1]);
+      }
+    }
+  }, [product.images]);
 
   // Prevent modal opening when clicking on interactive elements inside the card
   const handleArticleClick = (e: React.MouseEvent) => {
@@ -46,7 +59,17 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
           className={styles.imageWrapper}
           aria-label={`View ${product.title}`}
         >
-          <Image
+          {albumGroupId && (
+            <UploadcareImage
+              alt={`Product image 1`}
+              src={`https://ucarecdn.com/${albumGroupId}/nth/1/`}
+              width={200}
+              height={200}
+              sizes="(max-width: 600px) 100vw, 180px"
+            />
+          )}
+
+          {/* <Image
             src={
               product.mainImage
                 ? `/${product.mainImage}`
@@ -58,7 +81,7 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
             sizes="(max-width: 600px) 100vw, 180px"
             priority
             draggable={false}
-          />
+          /> */}
         </div>
         <div className={styles.cardContent}>
           <div className={styles.title} tabIndex={0}>
