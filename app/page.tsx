80fcecf4d6c031b83fd407228bc372ac831f7bf5
Sasisda228 <43885390+Sasisda228@users.noreply.github.com>
2025-05-04 20:18:54 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Counter from "@/components/counter";
+import Counter from "@/components/Counter";
 import LegalInfo from "@/components/LegalInfo";
 import ProductCarousel from "@/components/ProductCarousel";
 import ReviewsSection from "@/components/ReviewsSection";
