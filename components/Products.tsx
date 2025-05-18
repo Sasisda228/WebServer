ac6bb1d407df80347da32892d1c8ff93ed63eb0e
Sasisda228 <43885390+Sasisda228@users.noreply.github.com>
2025-05-18 10:27:44 +0300

    Update Products.tsx

--- components/Products.tsx
+++ components/Products.tsx
@@ -45,16 +45,18 @@ const Products = ({ slug }: any) => {
   }
 
   return (
-    <div className="grid grid-cols-3 justify-items-center max-lg:gap-x-2 gap-x-64 gap-y-5 max-[1300px]:grid-cols-3 max-lg:grid-cols-2 max-[500px]:grid-cols-2 ">
-      {products.length > 0 ? (
-        products.map((product: Product) => (
-          <ProductItem key={product.id} product={product} />
-        ))
-      ) : (
-        <h3 className="text-3xl mt-5 text-center w-full col-span-full max-[1000px]:text-2xl max-[500px]:text-lg">
-          No products found for specified query
-        </h3>
-      )}
+    <div className="justify-center">
+      <div className=" grid grid-cols-3 justify-items-center max-lg:gap-x-2 gap-x-64 gap-y-5 max-[1300px]:grid-cols-4 max-lg:grid-cols-2 max-[500px]:grid-cols-2 ">
+        {products.length > 0 ? (
+          products.map((product: Product) => (
+            <ProductItem key={product.id} product={product} />
+          ))
+        ) : (
+          <h3 className="text-3xl mt-5 text-center w-full col-span-full max-[1000px]:text-2xl max-[500px]:text-lg">
+            No products found for specified query
+          </h3>
+        )}
+      </div>
     </div>
   );
 };
