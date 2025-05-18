ff0b1276fa620a350be50ffd955e3a790b7b0cd1
Sasisda228 <43885390+Sasisda228@users.noreply.github.com>
2025-05-18 10:35:52 +0300

    Update page.tsx

--- app/shop/[[...slug]]/page.tsx
+++ app/shop/[[...slug]]/page.tsx
@@ -10,12 +10,12 @@ const ShopPage = (slug: any) => {
     <div className={styles.root}>
       <div className={styles.container}>
         <div className={styles.divider}></div>
-        <div className={styles.titleWrapper}>
+        {/* <div className={styles.titleWrapper}>
           <h1 className={styles.title}>
             <span className={styles.titleGradient}>47STORE</span>
             <br />
           </h1>
-        </div>
+        </div> */}
         <div>
           <Filters />
           <Products slug={slug} />
