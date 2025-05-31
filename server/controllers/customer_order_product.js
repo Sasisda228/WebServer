43975026d0964dbffc1d51d006f0d852eb74c6a8
Sasisda228 <43885390+Sasisda228@users.noreply.github.com>
2025-05-31 16:11:40 +0300

    Update customer_order_product.js

--- server/controllers/customer_order_product.js
+++ server/controllers/customer_order_product.js
@@ -85,75 +85,77 @@ async function getProductOrder(request, response) {
 }
 
 
-async function getAllProductOrders(request, response) {
-    try {
-        // Getting all orders from customer_order_product table
-        const productOrders = await prisma.customer_order_product.findMany({
-            select: {
-                productId: true,
-                quantity: true,
-                customerOrder: {
-                    select: {
-                        id: true,
-                        name: true,
-                        lastname: true,
-                        phone: true,
-                        email: true,
-                        company: true,
-                        adress: true,
-                        apartment: true,
-                        postalCode: true,
-                        dateTime: true,
-                        status: true,
-                        total: true
-                    }
-                }
-            }
+async function getAllProductOrders(response) {
+  try {
+    // Getting all orders from customer_order_product table
+    const productOrders = await prisma.customer_order_product.findMany({
+      select: {
+        productId: true,
+        quantity: true,
+        customerOrder: {
+          select: {
+            id: true,
+            name: true,
+            lastname: true,
+            phone: true,
+            email: true,
+            company: true,
+            adress: true,
+            apartment: true,
+            postalCode: true,
+            dateTime: true,
+            status: true,
+            total: true,
+          },
+        },
+      },
+    });
+
+    // Creating map for storing data about orders grouped by customerOrderId
+    const ordersMap = new Map();
+
+    // Iterating through all orders
+    for (const order of productOrders) {
+      const { customerOrder, productId, quantity } = order;
+      const { id, ...orderDetails } = customerOrder;
+
+      // Finding a product from the table Product with productId
+      const product = await prisma.product.findUnique({
+        where: {
+          id: productId,
+        },
+        select: {
+          id: true,
+          title: true,
+          images: true,
+          price: true,
+          slug: true,
+        },
+      });
+
+      if (ordersMap.has(id)) {
+        // If there is input for customerOrderId, add a new product in existing map array
+        ordersMap.get(id).products.push({ ...product, quantity });
+      } else {
+        // Otherwise create new input for customerOrderId and add the product
+        ordersMap.set(id, {
+          customerOrderId: id,
+          customerOrder: orderDetails,
+          products: [{ ...product, quantity }],
         });
-
-        // Creating map for storing data about orders grouped by customerOrderId
-        const ordersMap = new Map();
-
-        // Iterating through all orders
-        for (const order of productOrders) {
-            const { customerOrder, productId, quantity } = order;
-            const { id, ...orderDetails } = customerOrder;
-
-            // Finding a product from the table Product with productId
-            const product = await prisma.product.findUnique({
-                where: {
-                    id: productId
-                },
-                select: {
-                    id: true,
-                    title: true,
-                    mainImage: true,
-                    price: true,
-                    slug: true
-                }
-            });
-
-            if (ordersMap.has(id)) {
-                // If there is input for customerOrderId, add a new product in existing map array
-                ordersMap.get(id).products.push({ ...product, quantity });
-            } else {
-                // Otherwise create new input for customerOrderId and add the product
-                ordersMap.set(id, {
-                    customerOrderId: id,
-                    customerOrder: orderDetails,
-                    products: [{ ...product, quantity }]
-                });
-            }
-        }
-
-        // Converting map to object array
-        const groupedOrders = Array.from(ordersMap.values());
-
-        return response.json(groupedOrders);
-    } catch (error) {
-        console.error('Error fetching all product orders:', error);
-        return response.status(500).json({ error: "Error fetching all product orders" });
+      }
     }
+
+    // Converting map to object array
+    const groupedOrders = Array.from(ordersMap.values());
+
+    return response.json(groupedOrders);
+  } catch (error) {
+    console.error("Error fetching all product orders:", error);
+    return response
+      .status(500)
+      .json({ error: "Error fetching all product orders" });
+  }
 }
 
 
